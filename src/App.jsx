eec7246d296be5b8bc21343debd95f194c4fd0bb
Gheorghita Hurmuz <hurmuz.gheorghita@pm.me>
2023-04-04 20:04:25 +0300

    remove unused code

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,6 @@ import "./App.css";
 import { PhoneTable } from "./PhoneTable";
 import Stack from "@mui/material/Stack";
 import Box from "@mui/material/Box";
-import { createTheme, ThemeProvider, Button } from "@mui/material";
-
-const theme = createTheme({
-  palette: {
-    primary: {
-      main: "#ff0000",
-    },
-  },
-});
 
 function App() {
   return (
@@ -26,10 +17,6 @@ function App() {
           <UpSertForm />
         </Box>
       </Stack>
-
-      <ThemeProvider theme={theme}>
-        <Button variant="contained">apasa</Button>
-      </ThemeProvider>
     </Box>
   );
 }
